6ad299b4ba8a0152d0e56155dfb663d1552c2374
Billy Barbaro <billy@thebarbaros.com>
2015-10-23 21:03:39 -0400

    Started project with example code provided from spec.

---
+++ main.h
@@ -0,0 +1,73 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+#include <sys/errno.h>
+#include <sys/time.h>
+#include <time.h>
+#include <sys/wait.h>
+
+#define SEMKEY 77
+#define SHMKEY 77
+
+#define NUM_SEMS   2
+#define SEM_MUTEX  0
+#define SEM_WAITLIST 1
+
+#define K 1024
+
+typedef struct customer {
+	int amount;
+} customer;
+
+typedef struct node {
+	customer value;
+	struct node *next;
+} node;
+
+typedef struct linked_list
+{
+	node head;
+	node tail;
+} linked_list;
+
+typedef struct common {
+	int wait_count;
+	int balance;
+} common;
+
+void P(int semid, int semaphore) {
+	struct sembuf psembuf;
+
+	psembuf.sem_op = -1;
+	psembuf.sem_flg = 0;
+	psembuf.sem_num = semaphore;
+	semop(semid, &psembuf, 1);
+	return;
+}
+
+void V(int semid, int semaphore) {
+	struct sembuf vsembuf;
+
+	vsembuf.sem_op = 1;
+	vsembuf.sem_flg = 0;
+	vsembuf.sem_num = semaphore;
+	semop(semid, &vsembuf, 1);
+	return;
+}
+
+union semun {
+	/* value for SETVAL */
+	int val;
+	/* buffer for IPC_STAT, IPC_SET */
+	struct semid_ds *buf;
+	/* array for GETALL, SETALL */
+	unsigned short *array;
+	/* Linux specific part: */
+	/* buffer for IPC_INFO */
+	struct seminfo *__buf;
+};
