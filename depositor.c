6bae3775a2fa902f012964b01b3c003f95cde5f5
Billy Barbaro <billy@thebarbaros.com>
2015-10-29 23:16:07 -0400

    Refactors to depositor

--- depositor.c
+++ depositor.c
@@ -1,5 +1,43 @@
 #include "main.h"
 
+int get_semid() {
+	int semid;
+	semid = semget(SEMKEY, NUM_SEMS, 0777);
+	if (semid < 0) {
+		perror("Could not get semaphores depositor");
+		exit(EXIT_FAILURE);
+  }
+	return semid;
+}
+
+struct common * get_shared() {
+	int shmid;
+
+	shmid = shmget(SHMKEY, 0, 0);
+	if (shmid < 0) {
+		perror("Could not get shared memory");
+		exit(EXIT_FAILURE);
+  }
+	return (struct common *)shmat(shmid, 0, 0);
+}
+
+struct customer* get_first_customer(int front_offset) {
+
+	int shmid;
+
+	shmid = shmget(front_offset, 0, 0);
+	if (shmid < 0) {
+		perror("Could not get shared memory");
+		exit(EXIT_FAILURE);
+  }
+	return (struct customer *)shmat(shmid, 0, 0);
+}
+
+void print_deposit_info(int amount, int balance) {
+	printf("Depositing %d dollars.\n", amount);
+	printf("Current balance %d dollars.\n", balance);
+}
+
 int main(int argc, char *argv[]) {
 
 	int semid, shmid;
@@ -8,46 +46,28 @@ int main(int argc, char *argv[]) {
 	struct customer *first_customer;
 
 	if (argc != 2) {
-		// Raise error and exit
+		perror("Please enter only one argument");
+		exit(EXIT_FAILURE);
 	}
 
-	// error check here
 	deposit_amount = atoi(argv[1]);
 
-	semid = semget(SEMKEY, NUM_SEMS, 0777);
-	if (semid < 0) {
-		perror("Could not get semaphores depositor");
-		exit(EXIT_FAILURE);
-  }
-	shmid = shmget(SHMKEY, 0, 0);
-	if (shmid < 0) {
-		perror("Could not get shared memory");
-		exit(EXIT_FAILURE);
-  }
-	shared = (struct common *)shmat(shmid, 0, 0);
+	semid = get_semid();
+	shared = get_shared();
 
+	// wait(mutex)
 	P(semid, SEM_MUTEX);
 
-	shmid = shmget(shared->front_of_line, 0, 0);
-	if (shmid < 0) {
-		perror("Could not get shared memory");
-		exit(EXIT_FAILURE);
-  }
-	first_customer = (struct customer *)shmat(shmid, 0, 0);
-
+	first_customer = get_first_customer(shared->front_of_line);
 	shared->balance = shared->balance + deposit_amount;
-
-	printf("Depositing %d dollars.\n", deposit_amount);
-	printf("Current balance %d dollars.\n", shared->balance);
-	if (shared->wait_count == 0) {
-		V(semid, SEM_MUTEX);
-	}
-	else if (first_customer_amount(first_customer) > shared->balance) {
+	print_deposit_info(deposit_amount, shared->balance);
+	if (shared->wait_count == 0 || first_customer_amount(first_customer) > shared->balance) {
+		// signal(mutex)
 		V(semid, SEM_MUTEX);
 	}
 	else {
+		//signal(wait_list)
 		V(semid, SEM_WAITLIST);
 	}
-
 	return EXIT_SUCCESS;
 }
